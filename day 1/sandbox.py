bb87484ac6db655e398182069b7569b62856e790
dimtay <dimtay@gmail.com>
2024-01-13 17:54:55 +0100

    init commit

---
+++ day 1/sandbox.py
@@ -0,0 +1,14 @@
+# new line
+print('Hello World!\nHello World!')
+
+# concatenation
+print("Hello" + " " + "Dima")
+
+# IndentationError
+# print("Hello" + " " + "Dima")
+
+# SyntaxError
+# print("Hello" + " " + "Dima)
+
+# NameError
+# rint("Hello" + " " + "Dima")
