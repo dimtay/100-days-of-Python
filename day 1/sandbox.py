284897b717f87aabc9d28b5c8af408be8c15519b
dimtay <dimtay@gmail.com>
2024-01-13 18:59:12 +0100

    more for day1

--- day 1/sandbox.py
+++ day 1/sandbox.py
@@ -1,8 +1,13 @@
-# new line
-print('Hello World!\nHello World!')
+# lesson 1
+# print('Hello World!\nHello World!')
+
+# lesson 2
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
 
 # concatenation
-print("Hello" + " " + "Dima")
+# print("Hello" + " " + "Dima")
 
 # IndentationError
 # print("Hello" + " " + "Dima")
@@ -15,3 +20,25 @@ print("Hello" + " " + "Dima")
 
 # NameError
 # rint("Hello" + " " + "Dima")
+
+# print("Hello " + input("What is your name"))
+
+# print(len(input()))
+
+# lesson 3
+# num1 = int(input())
+# num2 = int(input())
+# print(num1 * num2)
+
+# a = input()
+# b = input()
+
+# print("a: "  + a)
+# print("b: "  + b)
+
+print("Welcome to the Band Name Generator\n")
+
+city = input("City: ");
+pet = input("Pet: ");
+
+print("Alright! Your band name is: "  + city + " " + pet)
